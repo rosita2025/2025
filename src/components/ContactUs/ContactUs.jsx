ef8e7c9adac430e11648eaa02e482dbf961c6b39
rosita2025 <youtumundial2017@gmail.com>
2025-04-06 13:10:18 -0500

    Update ContactUs.jsx

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     return (
         <div className="contact-us-container">
             <h2>Contact Us</h2>
-            <p>Email: <a href="mailto:contact@ymfitrecipes.com">contact@ymfitrecipes.com</a></p>
-            <p>Phone: <a href="tel:+51972158956">+51972158956</a></p>
+            <p>Email: <a href="mailto:contact@vongee.com">contact@vongee.com</a></p>
+            <p>Phone: <a href="tel:+1-2818324261">+1-2818324261</a></p>
         </div>
     );
 };
